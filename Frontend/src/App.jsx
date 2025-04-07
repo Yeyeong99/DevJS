6fd0f670ecece0c7f4da20512a0c0d0c0a76ec45
Y00CHAN <kaiwow@naver.com>
2025-04-07 19:23:45 +0900

    New Frontend

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -2,7 +2,6 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from "./pages/Home";
 import Login from "./pages/Login";
-import Signup from "./pages/Signup";
 import Dashboard from './pages/Dashboard';
 import Upload from './pages/Upload';
 
@@ -17,7 +16,6 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/login" element={<Login />} />
-        <Route path="/signup" element={<Signup />} />
         <Route path="/dashboard" element={<Dashboard />} />
         <Route path="/upload" element={<Upload />} />
         <Route path="/kakao/callback" element={<KakaoCallback />} />
