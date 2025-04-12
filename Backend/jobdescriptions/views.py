17c5ed8c035b64451b2533502286cea3e9ffe58d
surin <tjdtnfls1128@gmail.com>
2025-04-13 00:39:37 +0900

    Update Model

--- Backend/jobdescriptions/views.py
+++ Backend/jobdescriptions/views.py
@@ -1,8 +1,13 @@
 # Create your views here.
 from rest_framework import viewsets, permissions
+from rest_framework.exceptions import ValidationError
 from .models import JobDescription, Skill
 from .serializers import JobDescriptionSerializer
 from .utils import extract_skills, ocr_naver
+
+# 디버깅
+import logging
+logger = logging.getLogger(__name__)
 
 
 # 파일 업로드, OCR 처리, JD 조회용 viewset 클래스
@@ -15,25 +20,29 @@ class JobDescriptionViewSet(viewsets.ModelViewSet):
         jd = serializer.save(user=self.request.user)    # jd에 사용자 넣어서 저장하기
         
         # 파일 받는 경우, 아닌 경우 나누기
-        if jd.file:    # 파일이 있다면,
+        if jd.file and jd.file.name:    # 파일이 있다면,
             
-            ocr_text = ocr_naver(jd.file.path)    # utils.py에서 함수 받아서 ocr text 받기
-            jd.content = ocr_text    # 원문 받기
-            jd.save()    # 다시 저장
+            try:
+                ocr_text = ocr_naver(jd.file.path)    # utils.py에서 함수 받아서 ocr text 받기
+                jd.content = ocr_text    # 원문 받기
             
-            main_skills = extract_skills(ocr_text)    # utils.py에서 함수 받아서 주요 역량 추출하기
-            for main_skill in main_skills:
-                skill = Skill(jd=jd, content=main_skill)
-                skill.save()
+            except Exception as e:
+                raise ValidationError('OCR 실패')
                 
         else:
-            main_skills = extract_skills(jd.content)
-            for main_skill in main_skills:
-                skill = Skill(jd=jd, content=main_skill)
-                skill.save()
+            ocr_text = jd.content    # 파일 없으면 원래 content 그래도 받기
             
+        jd.save()    # 다시 저장
+        
+        logger.info(f'JD created: {jd.id} | user: {jd.user} | company: {jd.company} | position: {jd.position}')    # 디버깅용 - 나중에 삭제해도 된다.
+            
+        main_skills = extract_skills(ocr_text)    # utils.py에서 함수 받아서 주요 역량 추출하기
+        for main_skill in main_skills:
+            skill = Skill(jd=jd, content=main_skill)
+            skill.save()
+    
 
     def get_queryset(self):
-        return JobDescription.objects.filter(user=self.request.user)
+        return JobDescription.objects.filter(user=self.request.user).order_by('-created_at')
 
 
